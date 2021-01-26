237964a85c873bbc41f4be9978fe7e77eb3afbd9
rashikapr <74289013+rashikapr@users.noreply.github.com>
2021-01-26 16:11:39 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Folder-creation
+# angryBirdsStage2.5
+Angry Birds stage 2.5 with Class Inheritance and Images
---
+++ slingshot.js
@@ -0,0 +1,27 @@
+class slingshot{
+    constructor(bodyA,pointB)
+{
+    var options={
+        bodyA:bodyA,
+        pointB:pointB,
+        stiffness:0.04,
+        length:10
+    }
+    this.slingshot=Constraint.create(options);
+    World.add(world,this.slingshot); 
+    this.pointB=pointB;  
+    
+}
+fly()
+    {
+        this.slingshot.bodyA=null;
+    }
+display(){
+    if(this.slingshot.bodyA){
+    var pointA = this.slingshot.bodyA.position;
+    var pointB= this. pointB;
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+
+}
+}
